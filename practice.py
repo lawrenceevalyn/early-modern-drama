bcdc5cb177495832b0da42aa3c1fd6459a492c02
lawrence <lawrenceevalyn@gmail.com>
2016-07-27 22:49:46 -0400

    set up a practice to read XML files

---
+++ practice.py
@@ -0,0 +1,38 @@
+# This program is just for trying out operations before they're added to lemmacounter.py
+
+import xml.sax
+
+# Define your specialized handler classes
+from xml.sax import saxutils
+
+class FindIssue(saxutils.DefaultHandler):
+    def __init__(self, title, number):
+        self.search_title, self.search_number = title, number
+        
+    def startElement(self, name, attrs):
+        # If it's not a comic element, ignore it
+        if name != 'comic': return
+
+        # Look for the title and number attributes (see text)
+        title = attrs.get('title', None)
+        number = attrs.get('number', None)
+        if (title == self.search_title and 
+	    number == self.search_number):
+            print title, '#' + str(number), 'found'
+
+# Create an instance of the handler classes
+dh = FindIssue('Sandman', '62')
+
+# Create an XML parser
+from xml.sax import make_parser
+parser = make_parser()
+
+# Tell the parser to use your handler instance
+parser.setContentHandler(dh)
+
+# Parse the file; your handler's methods will get called
+parser.parse(comic-practice.xml)
+
+
+# reference: the line of XML that has what I want:
+# <idno type="DLPS">A00456</idno>
