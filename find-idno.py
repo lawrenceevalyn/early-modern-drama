1659306b3f65f40366c034fe9bab281c051397d4
lawrence <lawrenceevalyn@gmail.com>
2016-08-08 18:25:32 -0400

    can filter idnos by type!
    use a loop to get the right idno (insert passive-aggressive comment
    about ElementTree here)

--- find-idno.py
+++ find-idno.py
@@ -4,14 +4,22 @@ fp = open("practice.xml","r")	#fp stands for file pointer, r stands for read
 tree = ET.parse(fp)
 root = tree.getroot()
 
-#idnos = root.findall('teiHeader')
-# prints "teiHeader"
+#finds all five idnos!!! but searches whole biblFull
+#idnos = root.findall('.//biblFull/publicationStmt/idno') 
+# also valid: root.findall('.//idno')
+# inexplicably not valid: root.findall('.//teiHeader/biblFull/publicationStmt/idno')
 
-idnos = root.findall('.//idno') #find all five idnos!!!
+idnos = root.findall(".//biblFull/publicationStmt/idno") 
 
-#idnos = root.findall('/teiHeader/biblFull/publicationStmt/idno')
+for idno in idnos :
+	if idno.attrib.get('type') == 'DLPS' :
+		print "found DLPS"
+		break # this immediately ends the loop and grabs the current value as "idno"
+	
+	else :
+		print "no DLPS"
 
-print idnos
+print idno
 
 #print len(idnos)
 
